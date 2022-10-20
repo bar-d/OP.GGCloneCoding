2ac79d21661835430cfdc36cc901eccafc1e5120
MinsuJeon <chso294@naver.com>
2022-10-20 17:46:16 +0900

    feat: SettingViewController 구현
    - SettingViewController 구현
    - SettingViewController를 TabBarController에 적용

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Settings/SettingsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SettingsViewController.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/10/20.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+
+    // MARK: Properties
+
+    private let tableView = SettingsTableView()
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupBackgroundColor()
+        setupSubviews()
+        setupConstraints()
+    }
+
+    // MARK: - Methods
+
+    private func setupBackgroundColor() {
+        view.backgroundColor = Design.backgroundColor
+    }
+
+    private func setupSubviews() {
+        view.addSubview(tableView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+
+// MARK: - Namespace
+
+private enum Design {
+    static let backgroundColor = UIColor(named: "PrimitiveColor")
+}
