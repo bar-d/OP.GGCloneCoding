1be9d739b6379bd2cf87a0b97460ffedebe6bcf7
bard <schy2503@naver.com>
2022-10-25 19:52:38 +0900

    feat: MainSection 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/Section/MainSection.swift
@@ -0,0 +1,18 @@
+//
+//  MainSection.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/10/24.
+//
+
+enum MainSection: Int ,CaseIterable {
+    case mySummoner
+    case favoriteSummoner
+    case patchNote
+    case favoriteChampions
+    case championTier
+    case todayTMI
+    case saleSkinsAndChampion
+    case anotherGame
+    case additionalInformation
+}
