99fa5fd45c0b125028644dee67104bdc9c77e8f8
bard <schy2503@naver.com>
2022-10-25 19:05:21 +0900

    feat: PatchNoteCell 구현
    - PatchNoteCell에 들어갈 PatchNoteImageView 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/SectionView/PatchNote/PatchNoteCell.swift
@@ -0,0 +1,147 @@
+//
+//  PatchNoteCell.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/10/24.
+//
+
+import UIKit
+
+final class PatchNoteCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = Design.titleLabelText
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let patchNoteScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    
+    private let patchNoteStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    private let firstPatchNoteImageView = PatchNoteImageView()
+    private let secondPatchNoteImageView = PatchNoteImageView()
+    private let thirdPatchNoteImageView = PatchNoteImageView()
+    
+    // MARK: - Initializers
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    // MARK: - Methods
+    
+    private func commonInit() {
+        setupSubviews()
+        setupConstraints()
+        setupContentInset(top: 0, left: 20, bottom: 0, right: 20)
+        setupBackgroundColor(UIColor(named: "PatchNoteColor"))
+        setupContentViewUserInteractionEnabled(false)
+        setupSelectionStyle(.none)
+    }
+    
+    private func setupSubviews() {
+        [titleLabel, patchNoteScrollView]
+            .forEach { addSubview($0) }
+        [patchNoteStackView]
+            .forEach { patchNoteScrollView.addSubview($0) }
+        [firstPatchNoteImageView, secondPatchNoteImageView, thirdPatchNoteImageView]
+            .forEach { patchNoteStackView.addArrangedSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        setupFirstPatchNoteImageViewConstraints()
+        setupTitleLabelConstraints()
+        setupPatchNoteScrollViewConstraints()
+        setupPatchNoteStackViewConstraints()
+    }
+    
+    private func setupFirstPatchNoteImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            firstPatchNoteImageView.heightAnchor.constraint(equalTo: titleLabel.widthAnchor, multiplier: 3/5),
+            firstPatchNoteImageView.widthAnchor.constraint(equalTo: firstPatchNoteImageView.heightAnchor, multiplier: 3/2),
+            secondPatchNoteImageView.heightAnchor.constraint(equalTo: firstPatchNoteImageView.heightAnchor),
+            secondPatchNoteImageView.widthAnchor.constraint(equalTo: firstPatchNoteImageView.widthAnchor),
+            thirdPatchNoteImageView.heightAnchor.constraint(equalTo: firstPatchNoteImageView.heightAnchor),
+            thirdPatchNoteImageView.widthAnchor.constraint(equalTo: firstPatchNoteImageView.widthAnchor)
+        ])
+    }
+    
+    private func setupTitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 20)
+            
+        ])
+    }
+    
+    private func setupPatchNoteScrollViewConstraints() {
+        NSLayoutConstraint.activate([
+            patchNoteScrollView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            patchNoteScrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            patchNoteScrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            patchNoteScrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    private func setupPatchNoteStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            patchNoteStackView.topAnchor.constraint(equalTo: patchNoteScrollView.topAnchor),
+            patchNoteStackView.bottomAnchor.constraint(equalTo: patchNoteScrollView.bottomAnchor, constant: -20),
+            patchNoteStackView.leadingAnchor.constraint(equalTo: patchNoteScrollView.leadingAnchor),
+            patchNoteStackView.trailingAnchor.constraint(equalTo: patchNoteScrollView.trailingAnchor),
+            patchNoteStackView.heightAnchor.constraint(equalTo: patchNoteScrollView.heightAnchor, constant: -20),
+        ])
+    }
+    
+    private func setupContentInset(top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat) {
+        patchNoteScrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+    }
+    
+    private func setupBackgroundColor(_ color: UIColor?) {
+        backgroundColor = color
+    }
+    
+    private func setupContentViewUserInteractionEnabled(_ bool: Bool) {
+        contentView.isUserInteractionEnabled = bool
+    }
+    
+    private func setupSelectionStyle(_ style: UITableViewCell.SelectionStyle) {
+        selectionStyle = style
+    }
+}
+
+// MARK: - Namespace
+
+private enum Design {
+    static let titleLabelText = "패치 노트"
+}
