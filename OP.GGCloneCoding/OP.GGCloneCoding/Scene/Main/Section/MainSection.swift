6d7923e9e539fe9b2c778036d283f02b0b341f8f
bard <schy2503@naver.com>
2022-10-25 19:59:01 +0900

    refactor: MainSection 리팩토링
    - MainSection 내 cell 프로퍼티 switch case 구문으로 구현

--- OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/Section/MainSection.swift
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/Section/MainSection.swift
@@ -5,6 +5,8 @@
 //  Created by 바드, 수꿍 on 2022/10/24.
 //
 
+import UIKit
+
 enum MainSection: Int ,CaseIterable {
     case mySummoner
     case favoriteSummoner
@@ -15,4 +17,27 @@ enum MainSection: Int ,CaseIterable {
     case saleSkinsAndChampion
     case anotherGame
     case additionalInformation
+    
+    var cell: UITableViewCell {
+        switch self {
+        case .mySummoner:
+            return MySummonerCell()
+        case .favoriteSummoner:
+            return FavoriteSummonersCell()
+        case .patchNote:
+            return PatchNoteCell()
+        case .favoriteChampions:
+            return FavoriteChampionCell()
+        case .championTier:
+            return MySummonerCell()
+        case .todayTMI:
+            return MySummonerCell()
+        case .saleSkinsAndChampion:
+            return ChampionsAndSkinsOnSaleCell()
+        case .anotherGame:
+            return MySummonerCell()
+        case .additionalInformation:
+            return MySummonerCell()
+        }
+    }
 }
