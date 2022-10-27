bd6d9bf35bc533c0b8b9d38dd9f037548f6cd0aa
bard <schy2503@naver.com>
2022-10-27 16:45:21 +0900

    feat: PositionTabBarCollectionViewCell 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/SectionView/ChampionTier/PositionTabBar/PositionTabBarCollectionViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  PositionTabBarCollectionViewCell.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/10/27.
+//
+
+import UIKit
+
+final class PositionTabBarCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    
+    private let titleLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(color: .lightGray)
+        .build()
+    
+    override var isSelected: Bool {
+        didSet{
+            titleLabel.textColor = isSelected ? .black : .lightGray
+        }
+    }
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    // MARK: - Methods
+    
+    func setupTitle(with text: String) {
+        titleLabel.text = text
+    }
+    
+    private func commonInit() {
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupSubviews() {
+        addSubview(titleLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
