251aff4e094d67542e8dd573eb326fab4924e2c1
bard <schy2503@naver.com>
2022-10-27 16:56:49 +0900

    feat: ChampionTierCollectionView 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/SectionView/ChampionTier/ChampionTierCollectionView.swift
@@ -0,0 +1,74 @@
+//
+//  ChampionTierCollectionView.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/10/27.
+//
+
+import UIKit
+
+final class ChampionTierCollectionView: UICollectionView {
+    
+    // MARK: Enumeration
+    
+    enum Section {
+        case tapBar
+        case page
+    }
+    
+    // MARK: - Properties
+
+    private let section: Section
+
+    // MARK: - Initializers
+    
+    init(section: Section) {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .horizontal
+        self.section = section
+        super.init(frame: .zero, collectionViewLayout: collectionViewLayout)
+
+       commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        self.section = .page
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    // MARK: - Methods
+    
+    private func commonInit() {
+        setupConstraintsAutomatic(false)
+        setupScrollIndicator(horizontal: false, vertical: false)
+        setupPagingEnable(true)
+        registerCell()
+    }
+    
+    private func setupConstraintsAutomatic(_ bool: Bool) {
+        translatesAutoresizingMaskIntoConstraints = bool
+    }
+    
+    private func setupScrollIndicator(horizontal: Bool, vertical: Bool) {
+        showsHorizontalScrollIndicator = horizontal
+        showsVerticalScrollIndicator = vertical
+    }
+    
+    private func setupPagingEnable(_ bool: Bool) {
+        isPagingEnabled = bool
+    }
+
+    private func registerCell() {
+        switch section {
+        case .tapBar:
+            register(PositionTabBarCollectionViewCell.self,
+                     forCellWithReuseIdentifier: PositionTabBarCollectionViewCell.identifier)
+        case .page:
+            register(ChampionTierCollectionViewCell.self,
+                     forCellWithReuseIdentifier: ChampionTierCollectionViewCell.identifier)
+        }
+    }
+}
+
