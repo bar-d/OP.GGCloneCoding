23cf58f4e935ed4ed9adb48065d8c166d006ca5a
MinsuJeon <chso294@naver.com>
2022-11-04 22:26:20 +0900

    feat: DefaultSummonerRepostiory 구현
    - DefaultSummonerRepostiory 구현을 위한 DTOError 생성

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/Repositories/DefaultSummonerRepostiory.swift
@@ -0,0 +1,40 @@
+//
+//  DefaultSummonerRepostiory.swift
+//  OP.GGCloneCoding
+//
+//  Created by 전민수 on 2022/11/04.
+//
+
+import Foundation
+
+struct DefaultSummonerRepostiory: SummonerRepository {
+    private let riotAPIService: RiotAPIService
+
+    init(riotAPIService: RiotAPIService = RiotAPIService()) {
+        self.riotAPIService = riotAPIService
+    }
+}
+
+extension DefaultSummonerRepostiory {
+    func fetchSummonerInformation(
+        id: String,
+        completion: @escaping (Result<Summoner, Error>) -> Void
+    ) {
+        let summonerRequest = RiotSummonerAPIRequest(summonerID: id)
+
+        riotAPIService.execute(summonerRequest) { result in
+            switch result {
+            case .success(let response):
+                guard let summonerInformation = response.toDomain() else {
+                    completion(.failure(DTOError.invalidTransformation))
+
+                    return
+                }
+
+                completion(.success(summonerInformation))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
