0b3ed26eebabb5dcbd1332db8831158d06a54c87
MinsuJeon <chso294@naver.com>
2022-11-10 17:04:21 +0900

    feat: RiotSummonerRankAPIRequest 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Infrastructure/Network/RiotAPI/RiotSummonerRankAPIRequest.swift
@@ -0,0 +1,22 @@
+//
+//  RiotSummonerRankAPIRequest.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/11/10.
+//
+
+import Foundation
+
+struct RiotSummonerRankAPIRequest: RiotAPIRequest {
+    typealias APIResponse = [RiotAPIResponseDTO.LeagueEntryDTO]
+
+    var httpMethod: HTTPMethod = .get
+    var path: URLPath?
+    var headers: [String : String]?
+    var body: Data?
+
+    init(encryptedSummonerID: String) {
+        path = .league(encryptedSummonerID: encryptedSummonerID)
+    }
+}
+
