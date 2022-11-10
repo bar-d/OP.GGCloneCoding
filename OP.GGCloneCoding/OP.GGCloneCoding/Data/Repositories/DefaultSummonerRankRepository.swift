42d21ba030bafb387a9a82c8bddbc690b619d232
MinsuJeon <chso294@naver.com>
2022-11-10 17:10:09 +0900

    feat: DefaultSummonerRankRepository 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/Repositories/DefaultSummonerRankRepository.swift
@@ -0,0 +1,52 @@
+//
+//  DefaultSummonerRankRepository.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/11/10.
+//
+
+import Foundation
+
+struct DefaultSummonerRankRepository: SummonerRankRepository {
+    private let riotAPIService: RiotAPIService
+
+    init(riotAPIService: RiotAPIService = RiotAPIService()) {
+        self.riotAPIService = riotAPIService
+    }
+}
+
+extension DefaultSummonerRankRepository {
+    func fetchSummonerInformation(
+        encryptedSummonerID: String,
+        completion: @escaping (Result<[SummonerRank], Error>) -> Void
+    ) {
+        let summonerRankRequest = RiotSummonerRankAPIRequest(encryptedSummonerID: encryptedSummonerID)
+
+        riotAPIService.execute(summonerRankRequest) { result in
+            switch result {
+            case .success(let response):
+                var array: [SummonerRank] = []
+
+                response.forEach { leagueEntryDTO in
+                    guard let summonerRankInformation = leagueEntryDTO.toDomain() else {
+                        completion(.failure(DTOError.invalidTransformation))
+
+                        return
+                    }
+
+                    array.append(summonerRankInformation)
+                }
+
+//                guard let summonerRankInformation = response.toDomain() else {
+//                    completion(.failure(DTOError.invalidTransformation))
+//
+//                    return
+//                }
+
+                completion(.success(array))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
