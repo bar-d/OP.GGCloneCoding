a6dbe3795a4ed42eeaa830f90490d91dbaeffc97
bard <schy2503@naver.com>
2022-11-22 21:03:56 +0900

    refactor: RiotSummonerMatchAPIRequest 리팩토링
    - APIResponse 타입 RiotAPIResponseDTO.MatchDTO로 변경

--- OP.GGCloneCoding/OP.GGCloneCoding/Infrastructure/Network/RiotAPI/RiotSummonerMatchAPIRequest.swift
+++ OP.GGCloneCoding/OP.GGCloneCoding/Infrastructure/Network/RiotAPI/RiotSummonerMatchAPIRequest.swift
@@ -8,13 +8,18 @@
 import Foundation
 
 struct RiotSummonerMatchAPIRequest: RiotAPIRequest {
-    typealias APIResponse = [String]
+    
+    typealias APIResponse = RiotAPIResponseDTO.MatchDTO
 
+    // MARK: Properties
+    
     var host: URLHost = .riotAsiaAPI
     var httpMethod: HTTPMethod = .get
     var path: URLPath?
     var headers: [String : String]?
     var body: Data?
+    
+    // MARK: - Initializers
 
     init(matchID: String) {
         path = .match(matchID: matchID)
