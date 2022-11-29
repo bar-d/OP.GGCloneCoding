01415b3b577c59f3fdc759afa424e6374e78b78e
bard <schy2503@naver.com>
2022-11-29 11:00:15 +0900

    feat: TierView 구현
    - 디테일 뷰 내 TierView 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/SummonderDetail/TierView/TierView.swift
@@ -0,0 +1,143 @@
+//
+//  TierView.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/11/09.
+//
+
+import SwiftUI
+
+final class TierView: UIView {
+    
+    // MARK: Properties
+    
+    private let tierIconImageView = ImageViewBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupImage(image: UIImage(named: "OP.GGIcon"))
+        .build()
+    
+    private let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .leading
+        
+        return stackView
+    }()
+    
+    private let queueTypeLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: " 개인/2인 랭크 ", color: UIColor(named: "LoadingView"))
+        .setupLayer(cornerRadius: 4)
+        .setupLayerBackgroundColor(UIColor(named: "ButtonFontColor"))
+        .build()
+    
+    private let tierLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: "Gold 2", color: .label)
+        .build()
+    
+    private let pointLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: "22 LP", color: .lightGray)
+        .build()
+    
+    private let winRateLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: "25승 32패 (40%)", color: .lightGray)
+        .build()
+    
+    private let indicatorButton = ButtonBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupImage(image: UIImage(systemName: "chevron.right"))
+        .setupColor(tint: .lightGray)
+        .build()
+    
+    // MARK: - Initilaizers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    // MARK: - Methods
+    
+    private func commonInit() {
+        setupConstraintsAutomatic(false)
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupConstraintsAutomatic(_ bool: Bool) {
+        translatesAutoresizingMaskIntoConstraints = bool
+    }
+    
+    private func setupSubviews() {
+        [queueTypeLabel, tierLabel, pointLabel, winRateLabel]
+            .forEach { labelStackView.addArrangedSubview($0) }
+        [tierIconImageView, labelStackView, indicatorButton]
+            .forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        setupTierIconImageViewConstraints()
+        setupLabelStackViewConstraints()
+        setupIndicatorButtonConstraints()
+    }
+    
+    private func setupTierIconImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            tierIconImageView.heightAnchor.constraint(
+                equalTo: heightAnchor, multiplier: 1/2
+            ),
+            tierIconImageView.widthAnchor.constraint(
+                equalTo: tierIconImageView.heightAnchor
+            ),
+            tierIconImageView.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 20
+            ),
+            tierIconImageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    private func setupLabelStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            labelStackView.leadingAnchor.constraint(
+                equalTo: tierIconImageView.trailingAnchor, constant: 20
+            ),
+            labelStackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            labelStackView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1/2)
+        ])
+    }
+    
+    private func setupIndicatorButtonConstraints() {
+        NSLayoutConstraint.activate([
+            indicatorButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            indicatorButton.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -20
+            )
+        ])
+    }
+}
+
+// MARK: - Preview
+
+struct TierView_Preview: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            let view = TierView()
+            view.translatesAutoresizingMaskIntoConstraints = true
+            
+            return view
+        }
+        .previewLayout(.fixed(width: 300, height: 150))
+    }
+}
