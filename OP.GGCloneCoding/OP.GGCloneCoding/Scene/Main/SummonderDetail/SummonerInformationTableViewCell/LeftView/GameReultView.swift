733fd4db3526295014b060590653597bbb948f24
bard <schy2503@naver.com>
2022-11-29 11:29:45 +0900

    feat: GameResultView 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Main/SummonderDetail/SummonerInformationTableViewCell/LeftView/GameReultView.swift
@@ -0,0 +1,124 @@
+//
+//  GameResultView.swift
+//  OP.GGCloneCoding
+//
+//  Created by 바드, 수꿍 on 2022/11/08.
+//
+
+import SwiftUI
+
+final class GameResultView: UIView {
+    
+    // MARK: Properties
+    
+    private let gameResultLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: "승", color: .systemBackground)
+        .build()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBackground
+        
+        return view
+    }()
+    
+    private let gameDurationLabel = LabelBuilder()
+        .setupConstraintsAutomatic(false)
+        .setupLabelText(text: "21:01", color: .systemBackground)
+        .setupLabelTextAttributes(font: .footnote)
+        .build()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    // MARK: - Methods
+    
+    private func commonInit() {
+        setupConstraintsAutomatic(false)
+        setupSubviews()
+        setupConstraints()
+        setupBackgroundColor()
+    }
+    
+    private func setupConstraintsAutomatic(_ bool: Bool) {
+        translatesAutoresizingMaskIntoConstraints = bool
+    }
+    
+    private func setupSubviews() {
+        [gameResultLabel, separatorView, gameDurationLabel]
+            .forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        setupGameResultLabelConstraints()
+        setupSeparatorViewConstraints()
+        setupGameDurationLabelConstraints()
+    }
+    
+    private func setupGameResultLabelConstraints() {
+        NSLayoutConstraint.activate([
+            gameResultLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gameResultLabel.bottomAnchor.constraint(
+                equalTo: separatorView.topAnchor,
+                constant: -8
+            )
+        ])
+    }
+    
+    private func setupSeparatorViewConstraints() {
+        NSLayoutConstraint.activate([
+            separatorView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            separatorView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            separatorView.widthAnchor.constraint(
+                equalTo: gameResultLabel.widthAnchor,
+                multiplier: 3/2
+            ),
+            separatorView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+    
+    private func setupGameDurationLabelConstraints() {
+        NSLayoutConstraint.activate([
+            gameDurationLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gameDurationLabel.topAnchor.constraint(
+                equalTo: separatorView.bottomAnchor,
+                constant: 8
+            )
+        ])
+    }
+    
+    private func setupBackgroundColor() {
+        if gameResultLabel.text == "승" {
+            backgroundColor = UIColor(named: "WinColor")
+        } else {
+            backgroundColor = UIColor(named: "LoseColor")
+        }
+    }
+}
+
+// MARK: - Preview
+
+struct GameResultView_Preview: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            let view = GameResultView()
+            view.translatesAutoresizingMaskIntoConstraints = true
+            
+            return view
+        }
+        .previewLayout(.fixed(width: 100, height: 300))
+    }
+}
