335837b37d951d165dad33367c04f63638aa45fd
bard <schy2503@naver.com>
2022-11-29 12:24:51 +0900

    refactor: HeaderView, HeaderViewBuilder 리팩토링
    - 각 탭바 별로 다른 제약을 가지고 있는 HeaderView를 위한 리팩토링
    - 기존의 제약을 public 메서드로 따로 추출

--- OP.GGCloneCoding/OP.GGCloneCoding/Scene/Common/View/HeaderView.swift
+++ OP.GGCloneCoding/OP.GGCloneCoding/Scene/Common/View/HeaderView.swift
@@ -20,11 +20,15 @@ final class HeaderView: UIView {
         return stackView
     }()
     
-    private let titleLabel = LabelBuilder()
-        .setupConstraintsAutomatic(false)
-        .setupLabelText(color: .label, alpha: .zero)
-        .setupLabelTextAttributes(alignment: .center)
-        .build()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.textAlignment = .center
+        label.alpha = .zero
+        
+        return label
+    }()
     
     private let rightBarItem: UIStackView = {
         let stackView = UIStackView()
@@ -64,20 +68,38 @@ final class HeaderView: UIView {
         titleLabel.alpha = alpha
     }
     
-    func setupFirstLeftButtonImage(with image: UIImage?) {
+    func setupFirstLeftButtonImage(with image: UIImage?, tint: UIColor? = .none) {
         firstLeftButton.setImage(image, for: .normal)
+        firstLeftButton.tintColor = tint
     }
     
-    func setupSecondLeftButtonImage(with image: UIImage?) {
+    func setupSecondLeftButtonImage(with image: UIImage?, tint: UIColor? = .none) {
         secondLeftButton.setImage(image, for: .normal)
+        secondLeftButton.tintColor = tint
     }
     
-    func setupFirstRightButtonImage(with image: UIImage?) {
+    func setupFirstRightButtonImage(with image: UIImage?, tint: UIColor? = .none) {
         firstRightButton.setImage(image, for: .normal)
+        firstRightButton.tintColor = tint
     }
     
-    func setupSecondRightButtonImage(with image: UIImage?) {
+    func setupSecondRightButtonImage(with image: UIImage?, tint: UIColor? = .none) {
         secondRightButton.setImage(image, for: .normal)
+        secondRightButton.tintColor = tint
+    }
+    
+    func setupTitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 20)
+        ])
+    }
+    
+    func setupViewConstraints() {
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: leftBarItem.topAnchor),
+            bottomAnchor.constraint(equalTo: leftBarItem.bottomAnchor)
+        ])
     }
     
     private func commonInit() {
@@ -103,7 +125,6 @@ final class HeaderView: UIView {
     
     private func setupConstraints() {
         setupLeftBarItemConstraints()
-        setupTitleLabelConstraints()
         setupRightBarItemConstraints()
     }
     
@@ -115,20 +136,10 @@ final class HeaderView: UIView {
         ])
     }
     
-    private func setupTitleLabelConstraints() {
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 20)
-        ])
-    }
-    
     private func setupRightBarItemConstraints() {
         NSLayoutConstraint.activate([
             rightBarItem.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
-            rightBarItem.trailingAnchor.constraint(
-                equalTo: trailingAnchor,
-                constant: -20
-            ),
+            rightBarItem.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             rightBarItem.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             rightBarItem.widthAnchor.constraint(equalTo: leftBarItem.widthAnchor)
         ])
