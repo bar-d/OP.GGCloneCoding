afe03132dc9c1dace0e2b21ed8cfa57ea398a12f
MinsuJeon <chso294@naver.com>
2022-12-02 16:24:39 +0900

    add: CoreData 생성
    - SummonerMatchListEntity CoreData 생성

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/SummonerStorage/SummonerMatchListEntityStorage/Entity/SummonerMatchListEntity+CoreDataClass.swift
@@ -0,0 +1,12 @@
+//
+//  SummonerMatchListEntity+CoreDataClass.swift
+//  OP.GGCloneCoding
+//
+//  Created by 전민수 on 2022/12/02.
+//
+//
+
+import CoreData
+
+@objc(SummonerMatchListEntity)
+public class SummonerMatchListEntity: NSManagedObject { }
---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/SummonerStorage/SummonerMatchListEntityStorage/Entity/SummonerMatchListEntity+CoreDataProperties.swift
@@ -0,0 +1,46 @@
+//
+//  SummonerMatchListEntity+CoreDataProperties.swift
+//  OP.GGCloneCoding
+//
+//  Created by 전민수 on 2022/12/02.
+//
+//
+
+import CoreData
+
+extension SummonerMatchListEntity {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<SummonerMatchListEntity> {
+        return NSFetchRequest<SummonerMatchListEntity>(entityName: "SummonerMatchListEntity")
+    }
+
+    @NSManaged public var identifier: String
+    @NSManaged public var matchList: [String]
+    @NSManaged public var puuid: String
+}
+
+extension SummonerMatchListEntity {
+    class func toEntity(by data: [String], in context: NSManagedObjectContext) throws {
+        guard let mySummoner = UserDefaults.standard.string(forKey: "MySummoner"),
+              let request: NSFetchRequest<SummonerInformation> = CoreDataSummonerInformationStorage.shared.fetchRequest(by: mySummoner),
+              let storedData = CoreDataSummonerInformationStorage.shared.read(by: request) else {
+            return
+        }
+
+        let summonerMatchListEntity = SummonerMatchListEntity(context: context)
+
+        summonerMatchListEntity.setValue(
+            storedData.name.convertToIdentifier(),
+            forKey: "identifier"
+        )
+        summonerMatchListEntity.setValue(
+            storedData.puuid,
+            forKey: "puuid"
+        )
+        summonerMatchListEntity.setValue(
+            data,
+            forKey: "matchList"
+        )
+
+        try CoreDataStorage.shared.save(context)
+    }
+}
