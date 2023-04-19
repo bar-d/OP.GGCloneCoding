3d3408d9018499f1aa270ef48452aeea7d8294ab
bard <schy2503@naver.com>
2023-04-20 02:57:24 +0900

    feat: SummonerRankListStorage 프로토콜 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/SummonerRankListStorage/SummonerRankListStorage.swift
@@ -0,0 +1,16 @@
+//
+//  SummonerRankListStorage.swift
+//  OP.GGCloneCoding
+//
+//  Created by bard on 2023/04/20.
+//
+
+import Foundation
+
+protocol SummonerRankListStorage: AnyObject {
+    func save(_ summonerRankList: [SummonerRank])
+    func getResponse(
+        maxCount: Int,
+        completion: @escaping (Result<[SummonerRank], Error>) -> Void
+    )
+}
