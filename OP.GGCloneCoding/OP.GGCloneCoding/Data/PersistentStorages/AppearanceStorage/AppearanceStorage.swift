8594f946b7ae179bf2ff770e3ada9320fa52cad4
bard <schy2503@naver.com>
2023-04-20 13:27:18 +0900

    feat: AppearanceStorage 프로토콜 구현

---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/AppearanceStorage/AppearanceStorage.swift
@@ -0,0 +1,14 @@
+//
+//  AppearanceStorage.swift
+//  OP.GGCloneCoding
+//
+//  Created by bard on 2023/04/20.
+//
+
+import Foundation
+
+protocol AppearanceStorage: AnyObject {
+    func setAppearanceUnspecified()
+    func setAppearanceLightMode()
+    func setAppearanceDarkmode()
+}
---
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/UserDefaultsStorages/UserDefaultsAppearanceStorage.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsAppearanceStorage.swift
+//  OP.GGCloneCoding
+//
+//  Created by bard on 2023/04/20.
+//
+
+import Foundation
+
+final class UserDefaultsAppearanceStorage: AppearanceStorage {
+    
+}
