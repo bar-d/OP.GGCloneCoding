6b0fa09bdd0724d6501737a993c182a324fbcb66
bard <schy2503@naver.com>
2023-04-21 12:36:59 +0900

    feat: UserDefaultsAppearanceStorage 구현

--- OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/UserDefaultsStorages/UserDefaultsAppearanceStorage.swift
+++ OP.GGCloneCoding/OP.GGCloneCoding/Data/PersistentStorages/UserDefaultsStorages/UserDefaultsAppearanceStorage.swift
@@ -6,7 +6,49 @@
 //
 
 import Foundation
+import UIKit
 
-final class UserDefaultsAppearanceStorage: AppearanceStorage {
+final class UserDefaultsAppearanceStorage: UserDefaultsStorage {
     
+    // MARK: Properties
+    
+    private let maxStorageLimit: Int?
+    private let userDefaults: UserDefaults
+    
+    private let window: UIWindow? = {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        
+        return window
+    }()
+    
+    // MARK: - Initializers
+    
+    init(maxStorageLimit: Int? = nil, userDefaults: UserDefaults = .standard) {
+        self.maxStorageLimit = maxStorageLimit
+        self.userDefaults = userDefaults
+    }
+}
+
+// MARK: Extension
+
+extension UserDefaultsAppearanceStorage: AppearanceStorage {
+    func setAppearanceUnspecified() {
+        userDefaults.set(
+            Appearance.unspecified.rawValue,
+            forKey: UserDefaults.Key.appearance
+        )
+        window?.overrideUserInterfaceStyle = .unspecified
+    }
+    
+    func setAppearanceLightMode() {
+        userDefaults.set(Appearance.light.rawValue, forKey: UserDefaults.Key.appearance)
+        window?.overrideUserInterfaceStyle = .light
+    }
+    
+    func setAppearanceDarkmode() {
+        userDefaults.set(Appearance.dark.rawValue, forKey: UserDefaults.Key.appearance)
+        window?.overrideUserInterfaceStyle = .dark
+    }
 }
